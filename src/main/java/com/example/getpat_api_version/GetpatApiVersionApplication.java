b40520675521f8832832debea004c0ee95b7f2d8
Mabisjwk <ogabi6035.2@gmail.com>
2025-07-24 17:58:10 -0500

    first commit

---
+++ src/main/java/com/example/getpat_api_version/GetpatApiVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.getpat_api_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetpatApiVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetpatApiVersionApplication.class, args);
+	}
+
+}
